9df4ab8e239e9426447c381969079f75a005377c
lucas-bubanz <lucas.bubanz@herval.com.br>
2025-03-26 15:42:30 -0300

    Adicionado conceitos de comentários e Classe Console com métodos Write (s/ quebra de linha) e WriteLine(c/quebra de linha).

--- Csharp/01conceito/Program.cs
+++ Csharp/01conceito/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Comentário de uma linha
+/*
+Comentário de duas linhas
+*/
+
+//Console.Write() - escreve na tela sem quebra de linha
+//Console.WriteLine() - escreve na tela com quebra de linha
